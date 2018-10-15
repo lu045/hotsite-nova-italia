6072d6f421b63ee98949b0967cc9a2fa1774e384
Luis Fernando <serraluisfernando@gmail.com>
2018-10-15 11:15:04 -0300

    base da estrutura

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+
+
+//Task para o sass
+gulp.task('sass', function () {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+//Task para o watch
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.sass', ['sass']);
+});
+
+
+
+
+//Task Default
+gulp.task('default', ['sass', 'watch']);
+
