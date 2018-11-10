7445811cd70a32a6c3b8c2db675c8e50ec5ef4db
Luis Fernando <serraluisfernando@gmail.com>
2018-11-10 15:50:58 -0200

    Organizacao dos elementos JS

--- js/slider.js
+++ js/slider.js
@@ -40,8 +40,8 @@ $(document).ready(function(){
 	 	 $('.disc li:nth-child(5)').fadeIn('slow');
 	 });
 
-
-
+     //Efeito nos botoes de avanço
+     //===========Botão Avançar================//	 
 	 $('.next').click(function(){
 	 	 //Efeito de Avanço
 	 	 //===========Variaveis================//
@@ -65,34 +65,23 @@ $(document).ready(function(){
 
 	 });
 
-
+	 //===========Botão Voltar================//	
 	 $('.prev').click(function(){
-	 	 //Efeito de Avanço
+	 	 //Efeito de Retrocesso
 	 	 //===========Variaveis================//
 	 	 var currentSlid = 1;
 	 	 var slidList = $('.display li');
 	 	 var numberOflist = (slidList.length);
 	 	 var liWidth = $('.display li').outerWidth();
 	 	 //===========Efeito===================//
-	 	     if (numberOflist>currentSlid){
-	 	      	 
+	 	     if (numberOflist>currentSlid){	 	      	 
 	 	      	 $('.display li').first().before($('.display li').last().css({"margin-left":-liWidth}));
                  $('.display').css({"width":"99999%"});
 	 	      	 $('.display').animate({left:liWidth}, 800, function(){
 	 	      	 	 $('.display li').css({"margin-left":"0"});
 	 	      	 	 $('.display').css({"width":"auto"});
 	 	      	 	 $(this).css({"left":"0", "width":"auto"});
-	 	      	 });
-
-
-		      	 
-
-
-		      	 /*$('.display').css({"width":"99999%"}).animate({left:liWidth}, 800, function(){
-	 	      	 	 $('.display').css({"left":"0"});
-	 	      	 	 $(this).css({"left":"0", "width":"auto"});
-	 	      	 });*/
-	 	      	 	 	  	
+	 	      	 });	 	      	 	 	  	
 	 	 	 }else {
 	 	 	  	 alert("Insira mais imagens para continuar!");
 	 	 	  }
