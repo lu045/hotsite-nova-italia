48f12540587db91db56dceb86bd2653c2c71e0c6
Luis Fernando <serraluisfernando@gmail.com>
2018-11-10 15:44:38 -0200

    Correcoes no efeito de transicao das imagens no display

--- js/slider.js
+++ js/slider.js
@@ -42,25 +42,68 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-	 $('.display li:last').insertBefore('.dislplay li:first');
 	 $('.next').click(function(){
-	 	 //Efeito de avanço
-	 	 $('.display li').animate({"left":"-100%"}, 900, function(){
-	 	 	
-
-	 	 	
-	 	 });
+	 	 //Efeito de Avanço
+	 	 //===========Variaveis================//
+	 	 var currentSlid = 1;
+	 	 var slidList = $('.display li');
+	 	 var numberOflist = (slidList.length);
+	 	 var liWidth = $('.display li').outerWidth();
+	 	 //===========Efeito===================//
+	 	     if (numberOflist>currentSlid){
+	 	      	 $('.display').css({"width":"99999%"}).animate({left:-liWidth}, 800, function(){
+	 	      	 	 $('.display li').last().after($('.display li').first());
+	 	      	 	 $(this).css({"left":"0", "width":"auto"});
+	 	      	 });	 	 	  	
+	 	 	 }else {
+	 	 	  	 alert("Insira mais imagens para continuar!");
+	 	 	  }
 
 	 	 //Efeito de avançar descrição
 
 	 	 //Efeito de desativar miniaturas
 
 	 });
+
+
+	 $('.prev').click(function(){
+	 	 //Efeito de Avanço
+	 	 //===========Variaveis================//
+	 	 var currentSlid = 1;
+	 	 var slidList = $('.display li');
+	 	 var numberOflist = (slidList.length);
+	 	 var liWidth = $('.display li').outerWidth();
+	 	 //===========Efeito===================//
+	 	     if (numberOflist>currentSlid){
+	 	      	 
+	 	      	 $('.display li').first().before($('.display li').last().css({"margin-left":-liWidth}));
+                 $('.display').css({"width":"99999%"});
+	 	      	 $('.display').animate({left:liWidth}, 800, function(){
+	 	      	 	 $('.display li').css({"margin-left":"0"});
+	 	      	 	 $('.display').css({"width":"auto"});
+	 	      	 	 $(this).css({"left":"0", "width":"auto"});
+	 	      	 });
+
+
+		      	 
+
+
+		      	 /*$('.display').css({"width":"99999%"}).animate({left:liWidth}, 800, function(){
+	 	      	 	 $('.display').css({"left":"0"});
+	 	      	 	 $(this).css({"left":"0", "width":"auto"});
+	 	      	 });*/
+	 	      	 	 	  	
+	 	 	 }else {
+	 	 	  	 alert("Insira mais imagens para continuar!");
+	 	 	  }
+
+	 	 //Efeito de avançar descrição
+
+	 	 //Efeito de desativar miniaturas
+
+	 });
+
+
 
 
 
