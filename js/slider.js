2b85299f83c6227565b1d6f0c548b5e9d79116c5
Luis Fernando <serraluisfernando@gmail.com>
2018-11-12 17:07:48 -0200

    adicao do elemento de thumbnail

--- js/slider.js
+++ js/slider.js
@@ -6,6 +6,17 @@ $(document).ready(function(){
 	 	 $('.display li:nth-child(1)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(1)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(1)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Segunda Miniatura
@@ -14,6 +25,17 @@ $(document).ready(function(){
 	 	 $('.display li:nth-child(2)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(2)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(2)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Terceira Miniatura
@@ -22,6 +44,17 @@ $(document).ready(function(){
 	 	 $('.display li:nth-child(3)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(3)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(3)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Quarta Miniatura
@@ -30,51 +63,124 @@ $(document).ready(function(){
 	 	 $('.display li:nth-child(4)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(4)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(4)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
+
 	 });
 
 	 //Quinta Miniatura
 	 $('.thumbnail li:nth-child(5)').click(function(){
-	 	 $('.display li').hide();
+	   	 $('.display li').hide();
 	 	 $('.display li:nth-child(5)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(5)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(5)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Sexta Miniatura
 	 $('.thumbnail li:nth-child(6)').click(function(){
-	 	 $('.display li').hide();
+	   	 $('.display li').hide();
 	 	 $('.display li:nth-child(6)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(6)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(6)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Setima Miniatura
 	 $('.thumbnail li:nth-child(7)').click(function(){
-	 	 $('.display li').hide();
+	   	 $('.display li').hide();
 	 	 $('.display li:nth-child(7)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(7)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(7)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Oitava Miniatura
 	 $('.thumbnail li:nth-child(8)').click(function(){
-	 	 $('.display li').hide();
+	   	 $('.display li').hide();
 	 	 $('.display li:nth-child(8)').show();
 	 	 $('.text-order li').hide();
 	 	 $('.text-order li:nth-child(8)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(8)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
 	 //Nona Miniatura
-	 $('.thumbnail li:nth-child(10)').click(function(){
-	 	 $('.display li').hide();
-	 	 $('.display li:nth-child(10)').show();
+	 $('.thumbnail li:nth-child(9)').click(function(){
+	   	 $('.display li').hide();
+	 	 $('.display li:nth-child(9)').show();
 	 	 $('.text-order li').hide();
-	 	 $('.text-order li:nth-child(10)').fadeIn('slow');
+	 	 $('.text-order li:nth-child(9)').fadeIn('slow');
+	 	 $('.thumbnail li:nth-child(9)').focusin().css({"opacity":"1"});
+	 	 //=================Efeito de Tirar o Foco======================
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
 	 });
 
-	 //Efeito de avanço nas miniuaturas
+
+
+
+
+     //Efeito de reprodução automatica	
 
      
+
+
 
      //Efeito nos botoes de avanço
      //===========Botão Avançar================//	 
@@ -85,9 +191,11 @@ $(document).ready(function(){
 	 	 var slidList = $('.display li');
 	 	 var descList = $('.text-order li');
 	 	 var actList = $('.text-order .ativo');
+	 	 var thumbList = $('.thumbnail li');
 	 	 var numberOflist = (slidList.length);
 	 	 var numberOfdisc = (descList.length);
 	 	 var numberOfact = (actList.length);
+	 	 var numberOfthumb = (thumbList.length);
 	 	 var liWidth = $('.display li').outerWidth();
 	 	 //===========Efeito===================//
 	 	     if (numberOflist>currentSlid){
@@ -106,6 +214,26 @@ $(document).ready(function(){
 	 	 }else{
 	 	 	 alert("Insira mais slides para avançar!");
 	 	 }
+
+	 	 //Efeito de avançarr miniaturas
+	 	 if(numberOfthumb>currentSlid){
+	 	 	 $('.thumbnail').css({"width":"99999%"}).animate({"margin-left":"-222px"}, 300, function(){
+	 	 	 	 $('.thumbnail li').last().after($('.thumbnail li').first());
+	 	 	 	 $('.thumbnail li').first().focus().css({"opacity":"1"});
+	 	 	 	 $(this).css({"margin-left":"0", "width":"auto"});
+	 	 	 });	 	 	 
+	 	 }else{
+	 	 	 alert("Insira mais imagens para avançar!");
+	 	 }
+
+
+	 	 
+
+
+
+
+
+
 	 	 //Efeito de desativar miniaturas
          //===========Campo da Descrição================//
 	 	 $('.text-order li:nth-child(1)').hide();
@@ -131,6 +259,29 @@ $(document).ready(function(){
 	 	 $('.display li:nth-child(9)').hide();
 	 	 $('.display li:nth-child(10)').hide();
 	 	 $('.display li').show();
+
+	 	 //===========Campo da Miniaturas================//
+	 	 $('.thumbnail li:nth-child(1)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(2)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(3)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(4)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(5)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(6)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(7)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(8)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(9)').css({"opacity":"0.5"});
+	 	 $('.thumbnail li:nth-child(10)').css({"opacity":"0.5"});
+
+	 	 //Efeito avanço nas miniaturas
+
+
+
+
+
+
+
+
+	 	 
 	 });
 
 	 //===========Botão Voltar================//	
@@ -194,28 +345,6 @@ $(document).ready(function(){
 
 	 });
 
-
-
-
-
-
-
-
-
-	 
-
-
-
-
-	
-
-
-
-
-
-
-
-	
-
+	  
 });
 
