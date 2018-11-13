a32e56ff2e63c2eb4c33d06e6d390dc0918090a0
Luis Fernando <serraluisfernando@gmail.com>
2018-11-13 16:55:58 -0200

    ajustes no slider

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){	 
-	 //Oculta a tela de destaque
-	 $('.descript-active').hide();
-	 $('#ex1').hide();
-	 //Da destaque a miniatura clicada
-	 $('.thumbnail li img').click(function(){
-	 	 //========Variaveis=================
-	 	 var imageUrl = $(this).attr('src');
-	 	 var imageAlt = $(this).attr('alt');
-	 	 //=======Açoes da função============
-	 	 $('.display li').hide();
-	 	 $('#ex1').show();
-	 	 $('.descript-active').show();
-	 	 $('#ex1').attr('src', imageUrl);
-	 	 $('#ex1').attr('alt', imageAlt);
-	 });
-	
-     //Efeito de reprodução automatica	
+ 	 //Efeito de reprodução automatica	
 
      
 
@@ -52,26 +36,7 @@ $(document).ready(function(){
 	 	 	 $('.text-order li').fadeIn('slow');
 	 	 }else{
 	 	 	 alert("Insira mais slides para avançar!");
-	 	 }
-
-	 	 //Efeito de avançarr miniaturas
-	 	 if(numberOfthumb>currentSlid){
-	 	 	 $('.thumbnail').css({"width":"99999%"}).animate({"margin-left":"-222px"}, 300, function(){
-	 	 	 	 $('.thumbnail li').last().after($('.thumbnail li').first());
-	 	 	 	 $(this).css({"margin-left":"0", "width":"auto"});
-	 	 	 });	 	 	 
-	 	 }else{
-	 	 	 alert("Insira mais imagens para avançar!");
-	 	 }
-
-	 	 //Efeito de desativar ação das miniatueas
-         //===========Campo da Descrição================//
-	 	 $('.text-order li').first().show();
-	 	 //===========Campo da Display=================//
-	 	 $('.display li').show();
-	 	 //=========Oculta a tela de destaque=========//
-	 	 $('#ex1').hide();
-	 	 $('.descript-active').hide();	 	 
+	 	 } 	 
 	 });
 
 	 //===========Botão Voltar===================//	
@@ -108,29 +73,6 @@ $(document).ready(function(){
 	 	 }else{
 	 	 	 alert("Insira mais slides para avançar!");
 	 	 }
-
-	 	 //Efeito de retroceder miniaturas
-	 	 if(numberOfthumb>currentSlid){
-	 	 	 $('.thumbnail li').first().before($('.thumbnail li').last().css({"margin-left":"-222px"}));
-	 	 	 $('.thumbnail').css({"width":"99999%"}).animate({"margin-left":"222px"}, 300, function(){
-	 	 	 	 $('.thumbnail li').css({"margin-left":"0"});
-	 	      	 $('.thumbnail').css({"width":"auto"});
-	 	      	 $(this).css({"margin-left":"0", "width":"auto"});	 	 	 	
-	 	 	 }); 
-	 	 }else{
-	 	 	 alert("Deu Erro");
-	 	 }
-
-	 	 //Efeito de desativar ação das miniatueas
-         //===========Campo da Descrição================//
-	 	 $('.text-order li').first().show();
-	 	 //===========Campo da Display=================//
-	 	 $('.display li').show();
-	 	 //=========Oculta a tela de destaque=========//
-	 	 $('#ex1').hide();
-	 	 $('.descript-active').hide();
-	 });
-
-	  
+	 });	  
 });
 
