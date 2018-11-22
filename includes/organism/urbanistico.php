59e138303098e5ab936fa0200328122e2b1c5c7e
Luis Fernando <serraluisfernando@gmail.com>
2018-11-22 15:46:40 -0200

    continua a arrumar indentacoes PHP

--- includes/organism/urbanistico.php
+++ includes/organism/urbanistico.php
@@ -1,53 +1,51 @@
 <div class="altura-geral">
- 	     <section class="urbanistico" id="urbanistico">
-
- 	 	     <div class="cb2">
- 	 	 	     <div class="container">
- 	 	 	         <h1>Urbanistico</h1>
- 	 	 	     </div>
- 	 	     </div>
-
- 	 	     <div class="conturb">
- 	 	         <a href="imagens/urbanistico-nova-italia.png" target="_blank">
- 	 	             <img src="imagens/urbanistico.png" class="urbimg" alt="Projeto urbanistico">
- 	 	         </a>
- 	 	     </div>
-
- 	 	     <div class="container">	 	    
- 	 	         <div class="esp-emp">
- 	 	             <div class="row">
- 	 	             	 <div class="col-sm-4">
- 	 	         	         <div class="e-item-1">
- 	 	         	         	 <img src="imagens/lotes.png" alt="Lotes">
- 	 	         	         	 <h2>Lotes a partir de 360 m²</h2>
- 	 	         	         </div>
- 	 	         	     </div>
-
- 	 	         	     <div class="col-sm-4">
- 	 	         	         <div class="e-item-2">
- 	 	         	         	 <img src="imagens/area-verde.png" alt="Área Verde">
- 	 	         	         	 <h2>Área verde de 27.388,88 m²</h2>
- 	 	         	         </div>
- 	 	         	     </div>
-
- 	 	         	     <div class="col-sm-4">
- 	 	         	         <div class="e-item-3">
- 	 	         	         	 <img src="imagens/unidades.png" alt="Unidades">
- 	 	         	         	 <h2>296 Unidades</h2>
- 	 	         	         </div>
- 	 	         	     </div>
- 	 	             </div>
- 	 	         </div>
- 	 	     
- 	 	             <div class="botoes-urbcom">
- 	 	                 <a href="condominio-nova-italia-urbanistico.pdf" target="_blank">
- 	 	                     <button class="urbbax">Baixar Urbanistico</button>
- 	 	                 </a>
- 	 	                 <a href="condominio-nova-italia-planta.pdf" target="_blank">
- 	 	                     <button class="plabbax">Baixar a Planta</button>
- 	 	                 </a>
- 	 	           
- 	 	     </div>
- 	 	     </div>
- 	     </section>
+ <section class="urbanistico" id="urbanistico">
+ 	 <div class="cb2">
+ 	 	 <div class="container">
+ 	 	 	 <h1>Urbanistico</h1>
+ 	 	 </div>
  	 </div>
+
+ 	 <div class="conturb">
+ 	 	 <a href="imagens/urbanistico-nova-italia.png" target="_blank">
+ 	 	     <img src="imagens/urbanistico.png" class="urbimg" alt="Projeto urbanistico">
+ 	 	 </a>
+ 	 </div>
+
+ 	 <div class="container">	 	    
+ 	 	 <div class="esp-emp">
+ 	 	     <div class="row">
+ 	 	         <div class="col-sm-4">
+ 	 	         	 <div class="e-item-1">
+ 	 	         	     <img src="imagens/lotes.png" alt="Lotes">
+ 	 	         	     <h2>Lotes a partir de 360 m²</h2>
+ 	 	         	</div>
+ 	 	         </div>
+
+ 	 	         <div class="col-sm-4">
+ 	 	         	 <div class="e-item-2">
+ 	 	         	     <img src="imagens/area-verde.png" alt="Área Verde">
+ 	 	         	     <h2>Área verde de 27.388,88 m²</h2>
+ 	 	         	 </div>
+ 	 	         </div>
+
+ 	 	         <div class="col-sm-4">
+ 	 	         	 <div class="e-item-3">
+ 	 	         	     <img src="imagens/unidades.png" alt="Unidades">
+ 	 	         	     <h2>296 Unidades</h2>
+ 	 	         	 </div>
+ 	 	         </div>
+ 	 	     </div>
+ 	 	 </div>
+ 	 	     
+ 	 	 <div class="botoes-urbcom">
+ 	 	     <a href="condominio-nova-italia-urbanistico.pdf" target="_blank">
+ 	 	         <button class="urbbax">Baixar Urbanistico</button>
+ 	 	     </a>
+ 	 	     <a href="condominio-nova-italia-planta.pdf" target="_blank">
+ 	 	         <button class="plabbax">Baixar a Planta</button>
+ 	 	     </a> 	 	           
+ 	 	 </div>
+ 	 </div>
+ </section>
+</div>
