03805824dc2aa087ef6d3d69b310bc777df9c6e5
Luis Fernando <serraluisfernando@gmail.com>
2018-11-22 12:41:21 -0200

    Correcao no PHP

--- includes/atoms/video.php
+++
@@ -1,5 +0,0 @@
-<div class="vl">
- <div class="normal">
- 	 <iframe src="https://www.youtube.com/embed/eqke0GacFDY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
- </div>
-</div>
--- includes/molecules/mapa.php
+++
@@ -1,5 +0,0 @@
-<div id="map"></div>
-
-<script type="text/javascript" src="js/mapa.js"></script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBY2AsV7LjkOWuG7Fbqez9ASh-clDO1B9I&callback=initMap">
-</script>
