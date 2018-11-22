f0c8ec9bb2da5169ba6c413101764c6b0830b331
Luis Fernando <serraluisfernando@gmail.com>
2018-11-22 16:01:49 -0200

    Termino da conversao de HTML a PHP

--- includes/organism/info.php
+++ includes/organism/info.php
@@ -1,36 +1,35 @@
 <section class="informacoes">
- 	 	 <div class="leeds">
- 	 	 	 <div class="conteudo-final">
- 	 	 	     <div class="container">
- 	 	 	 	     <h1>Moderno e classico ao mesmo tempo, assim é o Condomínio Nova Itália</h1>
- 	 	 	 	     <div class="lazer-area"><i class="far fa-image"></i></div>
- 	 	 	 	     <p>Venha morar com mais segurança e qualidade de vida, venha para o Condomínio Nova Itália, o mais novo lançamento da Terroá Empreendimentos.</p>
- 	 	 	     </div>
- 	 	 	 </div>
+ <div class="leeds">
+ 	 <div class="conteudo-final">
+ 	 	 <div class="container">
+ 	 	 	 <h1>Moderno e classico ao mesmo tempo, assim é o Condomínio Nova Itália</h1>
+ 	 	 	 <div class="lazer-area"><i class="far fa-image"></i></div>
+ 	 	 	 <p>Venha morar com mais segurança e qualidade de vida, venha para o Condomínio Nova Itália, o mais novo lançamento da Terroá Empreendimentos.</p>
  	 	 </div>
+ 	 </div>
+ </div>
 
- 	 	 <div class="infovend">
- 	 	 	 <div class="conteudo-final2">
- 	 	 	     <div class="container">
- 	 	 	 	     <h1>Quer mais informações?</h1>
- 	 	 	 	     <p>Preencha o formulário abaixo, entraremos em contato com você!</p>
+ <div class="infovend">
+ 	 <div class="conteudo-final2">
+ 	 	 <div class="container">
+ 	 	 	 <h1>Quer mais informações?</h1>
+ 	 	 	 <p>Preencha o formulário abaixo, entraremos em contato com você!</p>
 
- 	 	 	 	     <form>
+ 	 	 	 <form>
+ 	 	 	 	 <label>
+ 	 	 	 	 	<input class="i1c"  type="text" name="Nome" placeholder="Nome">		
+ 	 	 	 	 </label>
+ 	 	 	 	 <label>
+ 	 	 	 	 	<input class="i2c" type="e-mail" name="E-mail" placeholder="E-mail">
+ 	 	 	 	 </label>
 
- 	 	 	 	 	     <label>
- 	 	 	 	 	 	     <input class="i1c"  type="text" name="Nome" placeholder="Nome">	 	 	 	 	 	
- 	 	 	 	 	     </label>
- 	 	 	 	 	     <label>
- 	 	 	 	 	 	     <input class="i2c" type="e-mail" name="E-mail" placeholder="E-mail">	 		
- 	 	 	 	 	     </label>
+ 	 	 	 	 <label>
+ 	 	 	 	 	<input class="i3c" type="text" name="Telefone" placeholder="Telefone">
+ 	 	 	 	 </label>
 
- 	 	 	 	 	     <label>
- 	 	 	 	 	 	     <input class="i3c" type="text" name="Telefone" placeholder="Telefone">		 	 	 	
- 	 	 	 	 	     </label>
-
- 	 	 	 	 	     <button class="envio">Enviar</button>
- 	 	 	 	     </form>
- 	 	 	     </div>
- 	 	 	 </div>
- 	 	 </div>		 	
- 	 </section>
+ 	 	 	 	 <button class="envio">Enviar</button>
+ 	 	 	 </form>
+ 	 	 </div>
+ 	 </div>
+ </div>		 	
+</section>
