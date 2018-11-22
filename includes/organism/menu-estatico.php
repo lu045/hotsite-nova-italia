b384b6626bded63d94fe8d648708a8de305af162
Luis Fernando <serraluisfernando@gmail.com>
2018-11-22 15:24:34 -0200

    Ajustes de indentacoes

--- includes/organism/menu-estatico.php
+++ includes/organism/menu-estatico.php
@@ -10,13 +10,13 @@
  	 	 <div class="tg"></div>
  	 	 <div class="tg"></div>
  	 </button>
- </div>
+</div>
 
- <div class="menu-toggle">
- 	 <ul>
- 	 	 <a href="#condominio"><li>O Condomínio</li></a>
-	     <a href="#urbanistico"><li>Urbanistico</li></a>
-	     <a href="#area-lazer"><li>Área de Lazer</li></a>
-	     <a href="#localizacao"><li>Localização</li></a>
- 	 </ul>
- </div>
+<div class="menu-toggle">
+ 	<ul>
+ 	 	<a href="#condominio"><li>O Condomínio</li></a>
+	    <a href="#urbanistico"><li>Urbanistico</li></a>
+	    <a href="#area-lazer"><li>Área de Lazer</li></a>
+	    <a href="#localizacao"><li>Localização</li></a>
+ 	</ul>
+</div>
--- includes/organism/navegacao-responsiva.php
+++ includes/organism/navegacao-responsiva.php
@@ -1,20 +1,20 @@
  <div class="principal-responsivo">
-	         	 <div class="logo-resp">
-	         	 	 <a href="index.html">
-	         	 	     <img src="imagens/novaitalia.svg" alt="Logo Nova Itália">
-	         	 	 </a>
-	         	 </div>
+ <div class="logo-resp">
+	 <a href="index.html">
+	     <img src="imagens/novaitalia.svg" alt="Logo Nova Itália">
+	 </a>
+ </div>
 	         	 
-	         	 <h1>Tudo que você deseja em um só lugar</h1>
+ <h1>Tudo que você deseja em um só lugar</h1>
 	         	
-	         	 <div class="menu-est-resp">
-	         	 	 <nav>
-	         	 	 	 <ul>
-	         	 	 	 	<a href="#condominio"><li>O Condomínio</li></a>
-	         	 	 	 	<a href="#urbanistico"><li>Urbanistico</li></a>
-	         	 	 	 	<a href="#area-lazer"><li>Área de Lazer</li></a>
-	         	 	 	 	<a href="#localizacao"><li>Localização</li></a>
-	         	 	 	 </ul>
-	         	 	 </nav>
-	         	 </div>
-	         </div>
+ <div class="menu-est-resp">
+	 <nav>
+	     <ul>
+	         <a href="#condominio"><li>O Condomínio</li></a>
+	         <a href="#urbanistico"><li>Urbanistico</li></a>
+	         <a href="#area-lazer"><li>Área de Lazer</li></a>
+	         <a href="#localizacao"><li>Localização</li></a>
+	     </ul>
+     </nav>
+ </div>
+</div>
